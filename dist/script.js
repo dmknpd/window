74b3c72b37b3814a2dbeb23309859ff1fa988adc
dmknpd <soupuabt@gmail.com>
2024-04-25 15:48:59 +0300

    ready for start

--- dist/script.js
+++ dist/script.js
@@ -3,7 +3,7 @@ var __webpack_exports__ = {};
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
-console.log("1");
+console.log(1);
 /******/ })()
 ;
 //# sourceMappingURL=script.js.map
