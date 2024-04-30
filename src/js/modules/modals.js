f7f91a2ba8c21a4854c15621fcb2f5c15e06e575
dmknpd <soupuabt@gmail.com>
2024-04-30 16:38:56 +0300

    complete

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -9,7 +9,8 @@ const modals = (state) => {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector),
-      windows = document.querySelectorAll("[data-modal]");
+      windows = document.querySelectorAll("[data-modal]"),
+      scroll = calcScroll();
 
     trigger.forEach((item) => {
       item.addEventListener("click", (e) => {
@@ -26,6 +27,7 @@ const modals = (state) => {
 
           modal.style.display = "block";
           document.body.style.overflow = "hidden";
+          document.body.style.marginRight = `${scroll}px`;
           //   document.body.classList.add("modal-open");
         }
       });
@@ -39,6 +41,7 @@ const modals = (state) => {
 
       modal.style.display = "none";
       document.body.style.overflow = "";
+      document.body.style.marginRight = "0px";
       //   document.body.classList.remove("modal-open");
     });
 
@@ -50,6 +53,7 @@ const modals = (state) => {
         });
         modal.style.display = "none";
         document.body.style.overflow = "";
+        document.body.style.marginRight = "0px";
         // document.body.classList.remove("modal-open");
       }
     });
@@ -75,6 +79,21 @@ const modals = (state) => {
     }
   }
 
+  function calcScroll() {
+    let div = document.createElement("div");
+
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.overflowY = "scroll";
+    div.style.visibility = "hidden";
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+  }
+
   bindModal(
     ".popup_engineer_btn",
     ".popup_engineer",
@@ -94,7 +113,7 @@ const modals = (state) => {
     ".popup_calc_end",
     ".popup_calc_end_close",
     false,
-    ["type"]
+    ["profile"]
   );
 
   // showModalByTime(".popup", 60000);
